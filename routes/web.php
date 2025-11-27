550b6f57e9a0e5357da274c1891010731fb46eec
alexisarellano0259 <alexis.arellano0259@alumnos.udg.mx>
2025-11-27 15:31:30 -0600

    Añadimos la ruta de Tareas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\TareaController;
 
 Route::get('/', function () {
     return redirect()->route('alumnos.index');
 });
 
 Route::resource('alumnos', AlumnoController::class);
+
+Route::resource('tareas', TareaController::class);
